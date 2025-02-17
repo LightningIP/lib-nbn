649ecb8af724e089c8f70fe776ef14d1ab2dddde
Gavin J <gavinj@outlook.com.au>
2025-02-18 07:09:37 +1100

    Add new ILocation based on HSL files

--- src/location/structures/struct.LocationID.ts
+++ src/location/structures/struct.LocationID.ts
@@ -5,6 +5,6 @@ export const ZLocationID = () => z.string()
     .min(15, 'Location ID must be 15 characters long.')
     .max(15, 'Location ID must be 15 characters long.')
     .regex(/^LOC\d{12}$/, 'Location ID must be in the format LOC############')
-    .refine((locid) => validateAadhaar(locid.slice(3)), {
+    /*.refine((locid) => validateAadhaar(locid.slice(3)), {
         message: 'Location ID is invalid.'
-    });
+    })*/;
