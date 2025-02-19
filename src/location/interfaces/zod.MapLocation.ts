ad5682976407726b8439416412499827fa97c80a
Gavin J <gavinj@outlook.com.au>
2025-02-19 23:59:16 +1100

    .

---
+++ src/location/interfaces/zod.MapLocation.ts
@@ -0,0 +1,118 @@
+import { z } from "zod";
+import { ZLocationID } from "../structures/struct.LocationID";
+import { ZCSAID, ZGNAFID, ZPOIID, ZServiceabilityClass, ZServiceClassDescription } from "../../structures";
+import { ZUnitTypeCode } from "../structures/struct.UnitTypeCode";
+import { ZServiceClassReason } from "../structures/struct.ServiceClassReason";
+import { ZCoatDescriptor } from "../structures/struct.CoatDescriptor";
+import { ZRoadTypeCode } from "../structures/struct.RoadTypeCode";
+import { ZHFLServiceType } from "../structures/struct.ServiceType";
+import { ZLevelTypeCode } from "../structures/struct.LevelTypeCode";
+import { ZRoadSuffixCode } from "../structures/struct.RoadSuffixCode";
+import { ZStateTerritoryCode } from "../structures/struct.StateTerritoryCode";
+import { ZServiceLevelRegion } from "../structures/struct.ServiceLevelRegion";
+import { ZListingType } from "../structures/struct.ListingType";
+import { ZTechnologyType } from "../structures/struct.TechnologyType";
+import { ZPlannedTechnology } from "../structures/struct.PlannedTechnology";
+import { ZCoatReason } from "../structures/struct.CoatReason";
+
+
+export const IHFLLocation = z.strictObject({
+    id: ZLocationID(),
+    formattedAddress: z.string().nonempty(),
+
+    /**
+     * The service class, in numeric form, which describes the state of a serviceable location.
+     */
+    serviceClass: ZServiceabilityClass(),
+
+    /**
+     * The service class, in full textual form (e.g. "Serviceable by fibre, no drop or NTD in place").
+     */
+    serviceClassDescription: ZServiceClassDescription,
+
+    /**
+     * Additional descriptive text pertaining to the Service Class when a location is unserviceable.
+     */
+    serviceClassReason: ZServiceClassReason.nullable(),
+
+    /**
+     * The date that nbn was ready to connect end users with Access Seekers within this Rollout Region; In HFL this is actual RFS date and In PFL it is expected RFS date
+     */
+    readyForServiceDate: z.string().date().nullable(),
+
+    /**
+     * The type of nbn™ network access connection
+     */
+    technologyType: ZTechnologyType,
+
+    /**
+     * Indicates that nbn has determined to make a change of access technology
+     * at this premises and the technology that is planned. This field is empty
+     * if no plans exist or after a change
+     */
+    plannedTechnology: ZPlannedTechnology.nullable(),
+
+    /**
+     * Indicates whether the change was driven by external factors, such as
+     * RSPs/end-users paying for a change of technology (labelled as
+     * "customer-initiated"), or the result of nbn internal processes (labelled
+     * as "nbn-initiated") or part of Fibre Connect program (labelled as
+     * "on-demand").
+     */
+    coatReason: ZCoatReason.nullable(),
+
+    /**
+     * A field  to help RSPs identify why a particular location has changed
+     * technology, only applicable to nbn-initiated and customer-initiated COAT
+     * locations.
+     */
+    coatDescriptor: ZCoatDescriptor.nullable(),
+
+    localityName: z.string(),
+    postcode: z.number().positive().int().nullable(),
+    stateTerritoryCode: ZStateTerritoryCode,
+    latitude: z.number({ coerce: true }),
+    longitude: z.number({ coerce: true }),
+
+    /**
+     * This field may be used by customers as an indicator of whether the location may be eligible for a New Development Charge upon completion of the first connect order at a New Development Location, as defined under the WBA. This field will be blank in PFL.
+     */
+    newDevelopmentsChargeApplies: z.boolean().nullable(),
+
+    /**
+     * The nbn service level application to this serviceable location
+     */
+    serviceLevelRegion: ZServiceLevelRegion,
+
+    poiIdentifier: ZPOIID().or(z.literal('Undefined')),
+    poiName: z.string(),
+    
+    
+    /**
+     * Indicates whether a premises is subject to the 'Co-existence Period' as outlined in the WBA.
+     */
+    coexistenceStatus: z.boolean().nullable().optional(),
+
+    lastUpdated: z.date({ coerce: true }),
+    
+    /**
+     * A premises is Ready to Connect when an nbn service order can be placed, and the service can be connected within an area that has been declared SRD.
+     */
+    rtcDate: z.string().date().nullable(),
+
+    /**
+     * The date at which the change of access technology build was completed and HFL values of Service Class, Service Class Description, Service Type and Technology Type for this premises were updated.
+     */
+    coatRtcDate: z.string().date().nullable(),
+
+    /**
+     * The date by which all services at the premises must be transitioned to the planned technology or nbn will commence the managed disconnection of services that have not been transitioned.
+     */
+    coatChangeoverDate: z.string().date().nullable(),
+
+    /**
+     * Indicates when nbn is planning to make the Planned Technology ready to connect. This is an indicative date only due to various factors that could affect construction processes.
+     */
+    coatForecastedRtcDate: z.string().nullable(),
+    
+});
--- src/location/structures/struct.CoatDescriptor.ts
+++ src/location/structures/struct.CoatDescriptor.ts
@@ -1,14 +1,65 @@
 import { z } from "zod";
 
-export const ZCoatDescriptor = z.enum([
-  "Fixed Wireless and Satellite Upgrade Program",
-  "Historical COAT Reasons",
-  "Assurance Fibre Upgrade",
-  "Network Activity Ticket - Underperforming Lines Flip",
-  "Regional Area Flip",
-  "Network Proactive Premises Flip",
-  "MDU Fibre Upgrade - Residential",
-  "MDU Fibre Upgrade - Business",
-  "Customer Funded - Individual Premises Flip",
-  "Never Connected and Inactive Premises Flip to Fibre",
-]);
+/**
+ * A field  to help RSPs identify why a particular location has changed technology, only applicable to nbn-initiated and customer-initiated COAT locations.
+ */
+export enum ECoatDescriptor {
+
+  /**
+   * Applies to locations in nbn’s Regional Satellite to Fixed Wireless upgrade program.
+   */
+  "Fixed Wireless and Satellite Upgrade Program" = "Fixed Wireless and Satellite Upgrade Program",
+
+  /**
+   * Applies to specific premises that have been unable to achieve 25 Mbps in the downlink and/or 5Mbps in the uplink or applies to locationswhere nbn has decided to resolve a Network Activity Ticket.
+   */
+  "Network Activity Ticket - Underperforming Lines Flip" = "Network Activity Ticket - Underperforming Lines Flip",
+
+  /**
+   * Applies to locations wherenbnhas made a decision to change the technology for a premises where a performance issue has been identified. (examples include but limited to speed, stability, repeat assurance issues)
+   */
+  "Assurance Fibre Upgrade" = "Assurance Fibre Upgrade",
+
+  /**
+   * Applies to locations where nbn makes strategic decisions to change the network for reasons such as network optimisation.
+   */
+  "Network Proactive Premises Flip" = "Network Proactive Premises Flip",
+  
+  /**
+   * Applies to residential premises that were upgraded under the Full Fibre Upgrade For Strata Program
+   */
+  "MDU Fibre Upgrade - Residential" = "MDU Fibre Upgrade - Residential",
+
+  /**
+   * Applies to business premises that were upgraded under the Full Fibre Upgrade For Strata Program
+   */
+  "MDU Fibre Upgrade - Business" = "MDU Fibre Upgrade - Business",
+
+  /**
+   * Applies to locations in a Regional Co-Investment Fund and Regional Connectivity Programs agreement. These technology changes usually apply to multiple locations in regional areas.
+   */
+  "Regional Area Flip" = "Regional Area Flip",
+
+  /**
+   * Applies to locations that have undergone a change of technology due to a Technology Choice Individual Premises Switch or Deals agreement.
+   */
+  "Customer Funded - Individual Premises Flip" = "Customer Funded - Individual Premises Flip",
+
+  /**
+   * Applies to locations where the underlining reason for the technology change was not identifiable or is unknown.
+   */
+  "Historical COAT Reasons" = "Historical COAT Reasons",
+
+  /**
+   * Applies to never connected and inactive premises that have been flipped to Fibre.
+   */
+  "Never Connected and Inactive Premises Flip to Fibre" = "Never Connected and Inactive Premises Flip to Fibre",
+}
+
+/**
+ * A field  to help RSPs identify why a particular location has changed technology, only applicable to nbn-initiated and customer-initiated COAT locations.
+ */
+export const ZCoatDescriptor = z.nativeEnum(ECoatDescriptor)
+  .describe(
+    'A field  to help RSPs identify why a particular location has changed technology, only applicable to nbn-initiated and customer-initiated COAT locations.'
+  );
